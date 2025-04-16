77a7c16bed2dd49f9f3a0425ee00c9775768f4a6
Fikremariam Mekonnen <fikremariam2012@gmail.com>
2025-04-16 13:00:46 +0300

    Resume updated

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio-Fikremariam/",
+  base: "/",
   server: {
     port: 3050,
   },
